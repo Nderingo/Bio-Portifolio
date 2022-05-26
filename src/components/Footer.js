a0fb6ef734760002a49913a65e39e757eda949af
Emmanuel Lyimo <esal29t@gmail.com>
2022-05-26 04:21:28 -0700

    updated the App.css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 import { MdEmail } from "react-icons/md";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
-// import { IoDocumentTextSharp } from "react-icons/io5"
 
 export default function Footer() {
   return (
@@ -16,9 +15,7 @@ export default function Footer() {
       <a href="http://linkedin.com/in/emmanuel-s-lyimo-4b53a996/" target="_blank" rel="noopener noreferrer"> 
         <h3> <BsLinkedin /> </h3>
       </a>
-      <a href="https://drive.google.com/file/d/1MEYN1a2HQp9i5H9hOEgbEBIaHD7J32v4/view?usp=sharing" target="_blank" rel="noopener noreferrer"> 
-        {/* <h3> <IoDocumentTextSharp /> </h3>  */}
-      </a>
+      
     </section>
   )
 }
