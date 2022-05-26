ca945023db18ca2eeb2f552ac7b0261c322dedbe
Emmanuel Lyimo <esal29t@gmail.com>
2022-05-26 04:09:27 -0700

    updated the bio.js and app.css

--- src/App.js
+++ src/App.js
@@ -2,26 +2,37 @@ import React from "react";
 
 
 import Nav from "./components/Nav";
-import About from "./components/About";
+import Bio from "./components/Bio";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
 import Resume from "./components/Resume";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect,useState } from "react"
+import './App.css'
 
 
 
 // In our main App component, we are rendering only single instances of Header and Navbar and several instances of Card
 function App() {
+  // Set view to about on page load
+  useEffect(() => {
+    setView('about');
+  }, []);
+
+  // Hash URL will load via direct link
+  const [ view, setView ] = useState(() => {
+    return document.location.hash.slice(1);
+  })
+
   return (
-    <div>
-      <Nav/>
-      <About />
-      <Projects/>
-      <Footer/>
-      <Contact/>
-      <Resume/>
-    </div>
+    <>
+      <Nav view={view} setView={setView} />
+      { view === "about" && <Bio /> }
+      { view === "work" && <Projects /> }
+      { view === "contact" && <Contact /> }
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Bio.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import esalPic from "../images/profile12.jpg";
+
+// Do we want to try this to organize the bio, pic, etc.
+// import { Card, CardGroup, Row, Col, Container } from "react-bootstrap/";
+
+import { SiHtml5, SiCss3, SiJavascript, SiMysql, SiSequelize, SiGraphql, SiMongodb, SiExpress, SiReact, SiNodedotjs, SiInsomnia, SiApollographql } from "react-icons/si"
+
+const ColoredLine = ({ color }) => (
+  <hr
+      style={{
+          color: color,
+          backgroundColor: color,
+          margin: 'auto',
+          width: '70%',
+          height: 5
+      }}
+  />
+);
+
+export default function Bio() {
+  return (
+    <section className='about-container mt-4 col-10'>
+        <div>
+          <p>Full stack web developer leveraging artistic and performance background to create thoughtful and inclusive experiences through code. Recent graduate of University of Washington’s Full Stack Flex Coding Boot Camp Program (partnership with Trilogy Education Services). My fellow students know me as a gregarious, sharp coder, who’s collaborative with code and debugging help. I helped lead teams to create an international holiday finder application and a healthy habits tracker. I am excited to employ my skillset in JavaScript, HTML, CSS, and more, to pivot my career and create better, healthier, more responsible, and honest spaces on the web.</p>
+          
+          <ColoredLine color="gray" />
+
+          <h4 className='nav-main nav-menu'>toolkit</h4>
+          <div className='tools-container col-10'>
+            
+            <h1><SiHtml5 alt="HTML"/></h1>
+            <h1><SiCss3 alt="CSS"/></h1>
+            <h1><SiJavascript alt="JS/Javascript"/></h1>
+
+            <h1><SiMongodb alt="MongoDB"/></h1>
+            <h1><SiExpress alt="Express"/></h1>
+            <h1><SiReact alt="React"/></h1>
+            <h1><SiNodedotjs alt="Node.js"/></h1>
+
+
+            {/* <h1><SiJquery alt="jQuery"/></h1> */}
+            <h1><SiMysql alt="MySQL"/></h1>
+            <h1><SiSequelize alt="Sequelize"/></h1>
+            <h1><SiGraphql alt="GraphQL"/></h1>
+            {/* <h1><SiHandlebarsdotjs alt="Handlebars"/></h1> */}
+            
+            {/* <h1><SiJest alt="Jest"/></h1> */}
+            <h1><SiInsomnia alt="Insomnia"/></h1>
+            {/* <h1><SiHeroku alt="Heroku"/></h1> */}
+            <h1><SiApollographql alt="Apollo"/></h1>
+
+          </div>
+        </div>
+
+        <div className='col-2 portfolio-picture'>
+          <img alt="Emma Lyimo - Full Stack Dev" src={esalPic} />
+        </div>
+    </section>
+  )
+}
